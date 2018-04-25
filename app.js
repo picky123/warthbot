e34fc2514a416d25442eead5c41127658820a34b
picky123 <38593029+picky123@users.noreply.github.com>
2018-04-25 21:09:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const token = 'NDM4MDYwOTkwNzY5MjY2Njg5.Db_JGQ.W_Vwr53KJK7fo5gjJTrsglQghAw';
+const token = ''NDM4MDYwOTkwNzY5MjY2Njg5.Db_JGQ.W_Vwr53KJK7fo5gjJTrsglQghAw'';
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
@@ -15,9 +15,14 @@ client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find('name', 'welcome');
   if (!channel) return;
   channel.send(`:hugging: Bun venit în comunitatea **WHART**, ${member}!
-:white_small_square: Ținând cont că ești o persoană nou venită pe serverul nostru, și dorim să te simti cat mai bine. 
-Ne para rau ca nu avem cel mai bun bot sau cel mai bun staff dar o sa ne dezvoltam pe parcurs.
-Te rog uitate pe **index** o sa te ajute destul de mult`);
+:small_blue_diamond: În primul, aș dori să vizitezi: 
+#index 
+#updates
+În primul rând vreau să îți multumesc că ai ales serverul **WHART.**
+Hai să îti prezentăm ce ai putea găsi pe server :
+:small_orange_diamond: Bot-ul serverului. 
+:small_orange_diamond: Un staff decent.
+:small_orange_diamond: Un mediu prietenos.`);
 });
 
 client.login('NDM4MDYwOTkwNzY5MjY2Njg5.Db_JGQ.W_Vwr53KJK7fo5gjJTrsglQghAw');
